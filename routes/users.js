0dc6192f9a54e2c776d5212178ea52fe2868428f
Your Name <you@example.com>
2021-11-18 01:24:03 -0500

    User CRUD

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+// Change to my api_1 when starting
+
+const express = require('express');
+const router = express.Router();
+const {protect, authorize} = require('../middleware/auth')
+const advancedResults = require('../middleware/advancedResults');
+const User = require('../models/user')
+
+
+const {
+    getUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+}= require('../controllers/users');
+
+router.use(protect)
+router.use(authorize('admin'))
+
+router.route('/').get(advancedResults(User).getUsers).post(createUser)
+
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser)
+
+module.exports = router; // not turning
--- server.js
+++ server.js
@@ -38,11 +38,13 @@ app.use(loggingMiddleware)
 const restaurantsRoutes = require('./routes/restaurants')
 const dishRoutes = require('./routes/dishes')
 const authRoutes = require('./routes/auth')
+const userRoutes= require('./routes/users')
 
     // Use   routes
     app.use('/api/v1/restaurants',restaurantsRoutes);// DO I need to change from plural to singular
     app.use('/api/v1/dishes',dishRoutes);
     app.use('/api/v1/auth', authRoutes);
+    app.use('/api/v1/users',userRoutes)
     
 // Error Handling Middleware- MUST BE LAST APP.USE();
 app.use(errorHandler);
